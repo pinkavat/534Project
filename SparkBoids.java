ca523b7744f3198e6e9cab3e890fae4903be4327
pinkavat <pinkavat@uw.edu>
2022-12-04 20:29:11 -0800

    uploading nonfunctional code

--- SparkBoids.java
+++ SparkBoids.java
@@ -27,6 +27,11 @@ public class SparkBoids {
 
     public static void main(String[] args) {
 
+        // Algorithm requires Boid flocking radius; however, it's stored in Boid.java as a squared
+        // radius, so we precompute the square root here for modularity.
+        double FLOCK_RADIUS = Math.sqrt(Boid.FLOCK_RADIUS_SQUARED) + 2.0;   // Add a bit of slop for safety's sake
+                                                                            // one never can tell with floats
+
         // Start Spark
         SparkConf sparkConf = new SparkConf()
             .setAppName("Boids Flocking Sim -- Spark Variant");
@@ -83,32 +88,116 @@ public class SparkBoids {
             System.err.println("");
 
 
+            // ========== BEGIN NAIVE APPROACH (because spatial hash is causing hangs) ==========
+           
+            /* 
+            JavaRDD<Boid> others = boids.map(boid -> {
+                return new Boid(boid.posX, boid.posY, boid.velX, boid.velY);
+            });
+        
+            JavaPairRDD<Boid, Boid> allPairs = boids.cartesian(others);
+            
+            boids = allPairs.map(boidSetPair ->{
+                ArrayList<Boid> b = new ArrayList<>(1);
+                b.add(boidSetPair._2);
+                boidSetPair._1.update(b);
+                return boidSetPair._1;
+            });
+            */
+            /*
+            JavaRDD<Set<Boid>> others = boids.map(boid -> {
+                Set<Boid>out = new HashSet<>(1);
+                out.add(new Boid(boid.posX, boid.posY, boid.velX, boid.velY));
+                return out;
+            });
 
-            // 1) Perform spatially hashed propagation: each Boid "receives" data from every neighbor in its
+            JavaPairRDD<Boid, Set<Boid>> allPairs = boids.cartesian(others);
+
+            allPairs = allPairs.reduceByKey((value1, value2) -> {
+                value1.addAll(value2);
+                return value1;
+            });
+
+            allPairs.foreach(boidSetPair -> {
+                boidSetPair._1.update(boidSetPair._2);
+            });
+
+            boids = allPairs.keys();
+            */
+        
+            // ========== BEGIN NONFUNCTIONAL SPATIAL HASHING APPROACH ==========
+            
+            // Begin spatial hashing by collecting the central chunks of each superchunk together, to
+            // make chunk traffic on a per-chunk rather than a per-boid basis
+            JavaPairRDD<Tuple2<Integer, Integer>, Set<Boid>> combinedCores = boids.flatMapToPair( boid -> {
+
+                List<Tuple2<Tuple2<Integer, Integer>, Set<Boid>>> flatOut = new ArrayList<>(1);
+
+                // Spatial hashing algorithm: space is divided into square chunks of flocking radius size, such that
+                // a Boid in a chunk cannot be flockmates with Boids in chunks outside the Moore Neighborhood of said chunk
+                // We therefore establish which chunk by dividing the Boid's position by the flock radius:
+                int chunkX = (int)(boid.posX / FLOCK_RADIUS);
+                int chunkY = (int)(boid.posY / FLOCK_RADIUS);
+
+                Set<Boid> core = new HashSet<>();
+                core.add(boid);
+
+                flatOut.add(new Tuple2(new Tuple2(Integer.valueOf(chunkX), Integer.valueOf(chunkY)), core));
+
+                return flatOut.iterator();
+            });
+
+
+            // Combine the core sets (simple append)
+            combinedCores = combinedCores.reduceByKey( (value1, value2) -> {
+                value1.addAll(value2);
+                return value1;
+            });
+
+
+            //    Perform spatially hashed propagation: each Boid "receives" data from every neighbor in its
             //    Moore neighborhood (i.e. all the other Boids with which it could possibly be flockmates) and
             //    "emits" its own data to all the Moore neighborhoods of which it is a part
             //
-            //  Dataset format: key, value pair where key is spatial region index and Value is a Tuple2 of
+            //  Dataset format: key, value pair where key is spatial region indexing tuple and Value is a tuple of
             //                  two sets of Boids: the first are the boids in the center of the region, the second are
             //                  all the boids in the region. Each boid emits nine messages, one of which contains itself
             //                  in the first set and the second set, the remainder of which only contain itself in the second set.
-            JavaPairRDD<Integer, Tuple2<Set<Boid>,Set<Boid>>> propagatedBoids = boids.flatMapToPair( boid -> {
-                List<Tuple2<Integer, Tuple2<Set<Boid>,Set<Boid>>>> flatOut = new ArrayList<>();
+            JavaPairRDD<Tuple2<Integer, Integer>, Tuple2<Set<Boid>,Set<Boid>>> propagatedBoids = combinedCores.flatMapToPair(kvPair -> {
                 
-                // TODO spatial hash
-                Set<Boid> core = new HashSet();
-                core.add(boid);
-                Set<Boid> surround = new HashSet();
-                // TODO NOTE: Data flows from the surround set to the core set, so they have to be INDEPENDENT in memory.
-                Boid dupe = new Boid(boid.posX, boid.posY, boid.velX, boid.velY);
-                surround.add(dupe);
-                flatOut.add(new Tuple2(Integer.valueOf(0), new Tuple2(core, surround)));
+                List<Tuple2<Tuple2<Integer, Integer>, Tuple2<Set<Boid>,Set<Boid>>>> flatOut = new ArrayList<>();
 
-                return flatOut.iterator(); 
-            });
+                Integer chunkX = kvPair._1._1;
+                Integer chunkY = kvPair._1._2;
 
-           
- 
+                // Duplicate message set
+                Set<Boid> dupe = new HashSet(kvPair._2.size());
+                for(Boid boid : kvPair._2){
+                    dupe.add(new Boid(boid.posX, boid.posY, boid.velX, boid.velY));
+                }
+
+                // The "core set" is the central chunk of the 9x9 chunks of the spatial hash; it contains 'true' boids,
+                // that receive data.
+                flatOut.add(new Tuple2(kvPair._1, new Tuple2(kvPair._2, dupe)));
+                                
+                // We create a dummy empty core for the messages to neighboring chunksets
+                Set<Boid> dummy = new HashSet();
+                
+                // Moore neighborhood
+                flatOut.add(new Tuple2(new Tuple2(Integer.valueOf(chunkX-1), Integer.valueOf(chunkY-1)),new Tuple2(dummy, dupe)));
+                flatOut.add(new Tuple2(new Tuple2(Integer.valueOf(chunkX-1), Integer.valueOf(chunkY)),  new Tuple2(dummy, dupe)));
+                flatOut.add(new Tuple2(new Tuple2(Integer.valueOf(chunkX-1), Integer.valueOf(chunkY+1)),new Tuple2(dummy, dupe)));
+                flatOut.add(new Tuple2(new Tuple2(Integer.valueOf(chunkX+1), Integer.valueOf(chunkY-1)),new Tuple2(dummy, dupe)));
+                flatOut.add(new Tuple2(new Tuple2(Integer.valueOf(chunkX+1), Integer.valueOf(chunkY)),  new Tuple2(dummy, dupe)));
+                flatOut.add(new Tuple2(new Tuple2(Integer.valueOf(chunkX+1), Integer.valueOf(chunkY+1)),new Tuple2(dummy, dupe)));
+                flatOut.add(new Tuple2(new Tuple2(Integer.valueOf(chunkX),   Integer.valueOf(chunkY-1)),new Tuple2(dummy, dupe)));
+                flatOut.add(new Tuple2(new Tuple2(Integer.valueOf(chunkX),   Integer.valueOf(chunkY+1)),new Tuple2(dummy, dupe)));
+
+                return flatOut.iterator();
+
+            }); 
+
+
             // 2) Collect neighborhoods together by key, combining the value boidsets. We therefore finish with
             //    one item per neighborhood, with a set of boids in its center, and a set of boids in its whole.
             
@@ -129,6 +218,8 @@ public class SparkBoids {
                 }
                 return value._1.iterator();
             });
+
+            
         }
 
 
